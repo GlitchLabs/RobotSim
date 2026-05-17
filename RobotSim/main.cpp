0ecd0cd93d5253da2e65c6a3c5d0f4536dfd5036
GlitchLabs <dakirthompson@gmail.com>
2026-05-16 21:20:17 -0400

    Initial commit for project nothign done yet

---
+++ RobotSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RobotSim
+//
+//  Created by Dakir Thompson on 5/16/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
